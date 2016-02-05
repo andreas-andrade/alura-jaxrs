2cff888e98e01c60baf6b354d1265512240db984
Andreas <andreasaw8@gmail.com>
2016-02-04 23:11:09 -0200

    colocando respostas http corretas nos resources

--- src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/resource/CarrinhoResource.java
@@ -1,12 +1,17 @@
 package br.com.alura.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.google.gson.Gson;
 import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
@@ -18,16 +23,17 @@ public class CarrinhoResource {
 	@Path("{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public String busca(@PathParam("id") long id) {
+	public Response busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
-		return carrinho.toJson();
+		return Response.ok(carrinho.toJson()).build();
 	}
 
 	@POST
-	@Produces(MediaType.APPLICATION_XML)
-	public String adiciona(String conteudo) {
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new Gson().fromJson(conteudo, Carrinho.class);
 		new CarrinhoDAO().adiciona(carrinho);
-		return "<status> Sucesso </status>";
+		URI uri = URI.create("/carrinhos/" + carrinho.getId());
+		return Response.created(uri).build();
 	}
 }
--- src/main/java/br/com/alura/loja/testes/ClienteTeste.java
+++ src/main/java/br/com/alura/loja/testes/ClienteTeste.java
@@ -2,26 +2,35 @@ package br.com.alura.loja.testes;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import com.thoughtworks.xstream.XStream;
+import com.google.gson.Gson;
 
 import br.com.alura.loja.Servidor;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import junit.framework.Assert;
 
 public class ClienteTeste {
 
 	private HttpServer server;
+	private WebTarget target;
+	private Client client;
 	
 	@Before
-	public void startaServidor(){ 
+	public void startaServidor(){
 		server = Servidor.inicializaServidor();
+
+		this.client = ClientBuilder.newClient();
+		this.target = client.target("http://localhost:8080");
 	}
 	@After
 	public void mataServidor() {
@@ -30,14 +39,31 @@ public class ClienteTeste {
 	
 	@Test
 	public void testaQueBuscarUmCarrinhoTrazOCarrinhoEsperado() {
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
 		
 		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		
 		// System.out.println(conteudo);
 		
-		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		Carrinho carrinho = (Carrinho) new Gson().fromJson(conteudo, Carrinho.class);
 		Assert.assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}
+	
+	@Test
+	public void testaQueSUportaNovosCarrinhos() {
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(314, "Microfone", 37, 40));
+		carrinho.setRua("Rua Tupaciguara 48");
+		carrinho.setCidade("Belo Horizonte");
+		String json = carrinho.toJson();
+		Entity<String> entity = Entity.entity(json, MediaType.APPLICATION_JSON);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		
+		Assert.assertEquals(201, response.getStatus());
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Microfone"));
+		
+	}
 }
+
